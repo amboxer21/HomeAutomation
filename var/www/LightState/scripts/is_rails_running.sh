96e3f35a92491c0554a2206cc38f685d7de809cd
Anthony Guevara <amboxer21@gmail.com>
2017-03-18 01:40:19 +0000

    Added crontab and bash script for it.

--- var/www/LightState/scripts/is_rails_running.sh
+++ var/www/LightState/scripts/is_rails_running.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
- 
-if [[ `ps aux | egrep --color -i "rails s -b 0.0.0.0" | wc -l` != 2 ]]; then 
-	/var/www/LightState/bin/rails s -b 0.0.0.0 -p 3000 &;
+
+export RAILS_ENV=production
+
+if [[ `ps aux | egrep --color -i "/var/www/LightState/bin/rails s -b 0.0.0.0" | wc -l` < 2 ]]; then 
+	/var/www/LightState/bin/rails s -b 0.0.0.0 -p 3000;
+fi
+
+if [[ `ps aux | egrep --color -i "/var/www/LightState/bin/rails" | wc -l` > 2 ]]; then 
+	kill -9 `ps aux | egrep --color -i "/var/www/LightState/bin/rails s -b 0.0.0.0"`;
 fi
